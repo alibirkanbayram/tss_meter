4d5b526f8db32af28caee7b11883724e84f5b4b7
Birkan <alibirkan9813@gmail.com>
2022-06-17 20:39:37 +0300

    • Update

--- main_code.ino
+++ main_code.ino
@@ -1,11 +1,36 @@
 // ━━━━━━━━━━━━━━━━━━━━━ Library ━━━━━━━━━━━━━━━━━━━━━
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>    //https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 // ━━━━━━━━━━━━━━━━━━━━━ Variable definition ━━━━━━━━━━━━━━━━━━━━━
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 int sensorPin = A0;
 float volt, ntu,acceptableValue= 0.5;
+
+// ━━━━━━━━━━━━━━━━━━━━━ EC Meter Variable definition ━━━━━━━━━━━━━━━━━━━━━
+int R1= 1000, Ra=25, Ecpin= A1, ECPower=A2,ECGround=A3 ;
+//*********** Converting to ppm [Learn to use EC it is much better**************//
+// Hana      [USA]        PPMconverion:  0.5
+// Eutech    [EU]          PPMconversion:  0.64
+//Tranchen  [Australia]  PPMconversion:  0.7
+// Why didnt anyone standardise this?
+float PPMconversion=0.7;
+float TemperatureCoef = 0.019; //this changes depending on what chemical we are measuring
+float K=2.88; // 2.9
+
+#define ONE_WIRE_BUS 10          // Data wire For Temp Probe is plugged into pin 10 on the Arduino
+const int TempProbePossitive =8;  //Temp Probe power connected to pin 9
+const int TempProbeNegative=9;    //Temp Probe Negative connected to
+OneWire oneWire(ONE_WIRE_BUS);// Setup a oneWire instance to communicate with any OneWire devices
+DallasTemperature sensors(&oneWire);// Pass our oneWire reference to Dallas Temperature.
+
+float Temperature=10, EC=0, EC25 =0, ppm =0, raw= 0, Vin= 5, Vdrop= 0, Rc= 0, buffer=0  ;
+
+// ━━━━━━━━━━━━━━━━━━━━━ EC Meter Variable definition ━━━━━━━━━━━━━━━━━━━━━
+int pH_Value, phPin=A4;
+
 
 // ━━━━━━━━━━━━━━━━━━━━━ Setup function ━━━━━━━━━━━━━━━━━━━━━
 void setup(){
@@ -14,6 +39,24 @@ void setup(){
     lcd.backlight();
     pinMode(doser1, OUTPUT);
     digitalWrite(doser1, LOW);
+
+    // EC Meter
+    pinMode(TempProbeNegative , OUTPUT ); //seting ground pin as output for tmp probe
+    digitalWrite(TempProbeNegative , LOW );//Seting it to ground so it can sink current
+    pinMode(TempProbePossitive , OUTPUT );//ditto but for positive
+    digitalWrite(TempProbePossitive , HIGH );
+    pinMode(ECPin,INPUT);
+    pinMode(ECPower,OUTPUT);//Setting pin for sourcing current
+    pinMode(ECGround,OUTPUT);//setting pin for sinking current
+    digitalWrite(ECGround,LOW);//We can leave the ground connected permanantly
+
+    delay(100);// gives sensor time to settle
+    sensors.begin();
+    delay(100);
+    R1=(R1+Ra);// Taking into acount Powering Pin Resitance
+
+    // PH Meter
+    pinMode(pH_Value, INPUT); 
 }
 
 // ━━━━━━━━━━━━━━━━━━━━━ Loop function ━━━━━━━━━━━━━━━━━━━━━
@@ -27,6 +70,9 @@ void loop(){
     lcd.print(values[1]);
     lcd.print(" NTU");
     delay(10);
+
+    getEC();
+    getPH();
 }
 
 // ━━━━━━━━━━━━━━━━━━━━━ Dosing function ━━━━━━━━━━━━━━━━━━━━━
@@ -62,4 +108,25 @@ float round_to_dp( float in_value, int decimal_place ){
   float multiplier = powf( 10.0f, decimal_place );
   in_value = roundf( in_value * multiplier ) / multiplier;
   return in_value;
+}
+void getEC(){
+    sensors.requestTemperatures();// Send the command to get temperatures
+    Temperature=sensors.getTempCByIndex(0); //Stores Value in Variable
+    digitalWrite(ECPower,HIGH);
+    raw= analogRead(ECPin);
+    raw= analogRead(ECPin);// This is not a mistake, First reading will be low beause if charged a capacitor
+    digitalWrite(ECPower,LOW);
+    Vdrop= (Vin*raw)/1024.0;
+    Rc=(Vdrop*R1)/(Vin-Vdrop);
+    Rc=Rc-Ra; //acounting for Digital Pin Resitance
+    EC = 1000/(Rc*K);
+    EC25  =  EC/ (1+ TemperatureCoef*(Temperature-25.0));
+    ppm=(EC25)*(PPMconversion*1000);
+    }
+
+void getPH(){
+    pH_Value = analogRead(phPin);
+    Voltage = pH_Value * (5.0 / 1023.0);
+    Serial.println(Voltage);
+    delay(500);
 }
